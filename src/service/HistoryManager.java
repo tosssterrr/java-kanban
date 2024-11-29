c6b0527d1408078461d7423a42a1f1ba6bd05333
tosssterrr <oops4534@gmail.com>
2024-11-29 18:31:01 +0300

    Изменить структуру интерфейса HistoryManager. Добавить метод remove, изменить возвращаемый тип у метода getHistory.

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -2,9 +2,10 @@ package service;
 
 import task.Task;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface HistoryManager {
     void add(Task task) ;
-    ArrayList<Task> getHistory();
+    void remove(int id) ;
+    List<Task> getHistory();
 }
