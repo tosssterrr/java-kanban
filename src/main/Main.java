74c447a5617d8e5e8edd5169052c7694614e50de
tosssterrr <oops4534@gmail.com>
2024-12-02 16:31:16 +0300

    Изменить реализацию InMemoryHistoryManager. Покрыть дополнительными тестами. Реализовать метод remove, изменить способы хранения и добавления задач. Написать тест на проверку порядка хранения.

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,59 +14,27 @@ public class Main {
                 TaskStatus.NEW, movingEpic);
         SubTask movingSubTask2 = new SubTask("Посуда", "Собрать посуду",
                 TaskStatus.NEW, movingEpic);
-        SubTask birthdaySubTask1 = new SubTask("Список гостей",
-                "Написать список приглашенных гостей", TaskStatus.IN_PROGRESS, birthdayEpic);
+        SubTask movingSubTask3 = new SubTask("Постельное белье",
+                "Собрать все белье", TaskStatus.IN_PROGRESS, movingEpic);
 
-        Task task1 = new Task("Покупка мебели", "Купить новую мебель для квартиры",
-                TaskStatus.IN_PROGRESS);
         // создаем задачи для тестирования программы
         taskManager.createEpic(movingEpic);
         taskManager.createEpic(birthdayEpic);
         taskManager.createSubTask(movingSubTask1);
         taskManager.createSubTask(movingSubTask2);
-        taskManager.createSubTask(birthdaySubTask1);
-        taskManager.createTask(task1);
+        taskManager.createSubTask(movingSubTask3);
         // получаем их для добавления в историю
-        taskManager.getTask(task1.getId());
-        taskManager.getTask(task1.getId());
-        taskManager.getTask(task1.getId());
-        taskManager.getTask(task1.getId());
-        taskManager.getTask(task1.getId());
         taskManager.getSubtask(movingSubTask1.getId());
         taskManager.getSubtask(movingSubTask2.getId());
         taskManager.getEpic(birthdayEpic.getId());
         taskManager.getEpic(movingEpic.getId());
+        taskManager.getSubtask(movingSubTask2.getId());
+        taskManager.getSubtask(movingSubTask3.getId());
+        System.out.println("История: " + taskManager.getHistory());
 
-
-
-        System.out.println("Задачи: " + taskManager.getTasks());
-        System.out.println("Подзадачи: " + taskManager.getSubtasks());
-        System.out.println("Эпики: " + taskManager.getEpics());
-
-
-        // обновляем статусы и меняем описание
-        task1.setStatus(TaskStatus.DONE);
-        movingSubTask1.setStatus(TaskStatus.DONE);
-        movingSubTask2.setStatus(TaskStatus.IN_PROGRESS);
-        taskManager.updateTask(task1);
-        taskManager.updateSubTask(movingSubTask1);
-        taskManager.updateSubTask(movingSubTask2);
-        movingEpic.setDescription("Задачи для переезда");
-        taskManager.updateEpic(movingEpic);
-        // проверяем действительно ли все изменилось
-        System.out.println("Статус 1-ой задачи - " + task1.getStatus());
-        System.out.println("Статус 1-ой подзадачи переезда - " + movingSubTask1.getStatus());
-        System.out.println("Статус 2-ой подзадачи переезда - " + movingSubTask2.getStatus());
-        System.out.println("Статус эпика переезда - " + movingEpic.getStatus());
         // удаляем задачи
-        taskManager.deleteTask(task1.getId());
         taskManager.deleteEpic(movingEpic.getId());
-
         // проверяем процесс удаления
-        System.out.println("Задачи: " + taskManager.getTasks());
-        System.out.println("Подзадачи: " + taskManager.getSubtasks());
-        System.out.println("Эпики: " + taskManager.getEpics());
-
         System.out.println(taskManager.getHistory());
 
 
